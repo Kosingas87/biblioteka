4112e632edf641618a30fe1da9ccdffba3f92f6b
stefanvasic <vaskesnajps@gmail.com>
2018-02-28 20:09:24 +0100

    napravljena klasa Biblioteka koja implementira interfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,36 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratoSveKnjige() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjige(Autor autor, String isbn, String naslov, String izdavac) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
